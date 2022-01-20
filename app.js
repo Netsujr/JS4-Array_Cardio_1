196da1345b6ac8c47573f71123d917f6ddb49a61
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-20 13:49:35 +0900

    mapped inventors names

--- app.js
+++ app.js
@@ -40,21 +40,37 @@ console.table(fifteen); //creates a table with info
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+//map alaways return the same amount of items as you give it
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.table(fullNames);
+
+
+// -----------------------------------------------------------------
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 
+// -----------------------------------------------------------------
+
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 
+// -----------------------------------------------------------------
+
 // 5. Sort the inventors by years lived
+
+// -----------------------------------------------------------------
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+// -----------------------------------------------------------------
+
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+
+// -----------------------------------------------------------------
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
