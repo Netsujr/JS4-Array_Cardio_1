d1e8f72a625133f3830a9be997fdc18e91ceb206
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-20 11:50:58 +0900

    filter refactored

--- app.js
+++ app.js
@@ -28,12 +28,15 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-const fifteen = inventors.filter(inventor => {
-  if (inventor.year >= 1500 && inventor.year <= 1600)
-    return true; //keep it
-});
-console.log(fifteen);
+
+const fifteen = inventors.filter(inventor =>
+  inventor.year >= 1500 && inventor.year <= 1600);
+    // return true; //keep it;
 console.table(fifteen); //creates a table with info
+// console.log(fifteen);
+
+// -----------------------------------------------------------------
+
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
